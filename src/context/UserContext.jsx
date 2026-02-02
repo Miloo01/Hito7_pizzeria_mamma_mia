6afc606a2c30b355194aa84ddc96eeb1c3cd41d9
Camilo <cmonsalsilva@gmail.com>
2026-02-02 20:27:13 -0300

    Hito_8 se implementa metodo login y register consumiendo rutas api

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -6,17 +6,55 @@ export const UserContext = createContext()
 
 //2 paso crear el provider
 const UserProvider = ({ children }) => {
-        //Estado inicial de token en true
-    const [token, setToken] = useState(true); // Simulación de estado de autenticación
+        //Estado inicial de token de true a null (porque al inicio no hay token)
+    const [token, setToken] = useState(null); 
+    const [email, setEmail] = useState(null);
+
+    // Estado 
     
-    // Método para iniciar sesión
-    const login = () => {
-        setToken(true);
+    // Método para iniciar sesión {Login}
+    const login = async(email, password) => {
+
+      const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        const data = await response.json();
+
+        if (data.token) {
+            setToken(data.token); // Guardar el token en el estado
+            setEmail(data.email); // Guardar el email en el estado
+        } else {
+            throw new Error(data.message || "Error al iniciar sesión");
+        }
+    };
+
+    // Método para registrar un usuario {Register}
+    const register = async (email, password) => {
+        const response = await fetch("http://localhost:5000/api/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        const data = await response.json();
+
+        if (data.token) {
+            setToken(data.token); // Guardar el token en el estado
+            setEmail(data.email); // Guardar el email en el estado
+        } else {
+            throw new Error(data.message || "Error al registrar usuario");
+        }
     };
 
     // Método para cerrar sesión
     const logout = () => {
-        setToken(false);
+      setToken(null);
+      setEmail(null);
     };
 
     const user = {
@@ -28,7 +66,7 @@ const UserProvider = ({ children }) => {
     
 
   return (
-    <UserContext.Provider value={{token, login, logout, user}}>
+    <UserContext.Provider value={{token, email, login, register ,logout}}>
       {children}
     </UserContext.Provider>
   )
